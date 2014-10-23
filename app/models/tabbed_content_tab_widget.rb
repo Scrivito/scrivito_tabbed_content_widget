b4764833494bc68f0921e65791c7c6ff0c39995a
Karina <kt@jetaido.com>
2014-10-23 12:02:36 +0200

    fixing some issues

--- app/models/tabbed_content_tab_widget.rb
+++ app/models/tabbed_content_tab_widget.rb
@@ -1,2 +1,9 @@
 class TabbedContentTabWidget < Widget
+
+  def additional_classes
+    if self.container.respond_to?("tabs") && self.container.tabs.first == self
+      "active"
+    end
+  end
+
 end
